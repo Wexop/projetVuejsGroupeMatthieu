735ad9d0bbad5f9d02866cc65f1fb14b10cb8053
EstebanBouts <estebanboutsbonhomme@gmail.com>
2023-12-21 11:47:14 +0100

    test

---
+++ src/views/GOView.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="game-over-container">
+        <div class="game-over-box">
+            <h1>GAME OVER</h1>
+            <p>SCORE : 2</p>
+            <div class="buttons">
+                <button @click="restartGame">RECOMMENCER</button>
+                <button @click="goHome">ACCUEIL</button>
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'GameOver',
+    methods: {
+        restartGame() {
+        },
+        goHome() {
+        }
+    }
+};
+</script>
+  
+<style scoped>
+.game-over-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+
+.game-over-box {
+    text-align: center;
+    border: 2px solid black;
+    padding: 20px;
+}
+
+.buttons {
+    display: flex;
+    justify-content: space-around;
+    margin-top: 20px;
+}
+
+button {
+    padding: 10px 20px;
+    cursor: pointer;
+}
+</style>
+  
