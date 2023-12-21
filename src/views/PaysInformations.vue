724c5c4bc347f2ad000400949023c17793d235a1
John Doe <mathieu.orieux@ecoles-epsi.net>
2023-12-21 10:09:39 -0500

    récupération des drapeaux

--- src/views/PaysInformations.vue
+++ src/views/PaysInformations.vue
@@ -1,9 +1,39 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div>
+    <h1>Liste des Drapeaux</h1>
+    <ul>
+      <li v-for="country in countries" :key="country.name.common">
+        {{ country.name.common }} - <img :src="country.flags.png" alt="Drapeau">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      countries: [],
+    };
+  },
+  mounted() {
+    this.fetchCountries();
+  },
+  methods: {
+    async fetchCountries() {
+      try {
+        const response = await axios.get('https://restcountries.com/v3.1/all?translations=fra');
+        this.countries = response.data;
+      } catch (error) {
+        console.error('Erreur lors de la récupération des pays', error);
+      }
+    },
+  },
+};
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+/* Vos styles CSS ici */
+</style>
