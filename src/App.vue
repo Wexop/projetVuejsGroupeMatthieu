b3e05d1608c6b034d5a1161dce43afce48d02f48
Wexop <mattheurtin@gmail.com>
2023-12-22 10:39:40 +0100

    update nav

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
     <header class="w-100">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <RouterLink class="navbar-brand" to="/game">Jeu des drapeaux</RouterLink>
-            <RouterLink class="navbar-brand" to="/paysList">Liste des drapeaux</RouterLink>
 
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <RouterLink class="navbar-brand" to="/game">Jeu des drapeaux</RouterLink>
+                    </li>
+                    <li class="nav-item">
+                        <RouterLink class="navbar-brand" to="/paysList">Liste des drapeaux</RouterLink>
+                    </li>
+                </ul>
+            </div>
         </nav>
     </header>
 
